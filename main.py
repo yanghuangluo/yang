8e67004da04294c4850e0a6241984e8e6f8a00f0
yanghuangluo <yangmingzhe2.0@outlook.com>
2026-08-16 17:38:19 +0800

    Update label text in MyTestApp

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MyTestApp(App):
     def build(self):
-        return Label(text="apk文件运行正常")
+        return Label(text="本apk文件运行正常")
 
 if __name__ == "__main__":
     MyTestApp().run()
